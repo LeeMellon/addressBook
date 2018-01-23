62a782daf4f1f6c35a873bcf9f6ae2514a2a6aaa
Ian Goodrich and Jared Clemmensen <jared@>
2018-01-22 16:41:14 -0800

    lost

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ function Contact(first, last, number, email) {
   this.last = last;
   this.number = number;
   this.email = email;
-  // this.pic = pic;
+
+
 }
 
 contacts = [];
@@ -18,21 +19,46 @@ $(function() {
     var email = $("#new-email").val();
 
     var newContact = new Contact(first, last, number, email);
-    contacts[last] = new Contact(first, last, number, email);
+      contacts.push(newContact);
 
-    $("#contact").clone().prependTo("#contactsList");
-    $("#contact").children("#name").text("Name: " + first + " " + last);
-    $("#moreInfo").children("#number").text("Number: " + number);
-    $("#moreInfo").children("#email").text("Email: " + email);
 
     $("#name").unbind().click(function() {
       $(this).siblings('span').toggle();
     });
 
     $("#delete").click(function() {
-      console.log("deleted");
-      $(this).closest("#contact").remove();
-    })
+      var number = this.getElementById("number").innerText;
+      console.log(typeof number);
+      var isolate = number.replace(/Number: /, "");
+      console.log(isolate);
+      for (var i=0; i<contacts.length; i++) {
+        var compare = "Number: " + contacts[i].number
+        if (compare === number) {
+          var removed = contacts.splice(i,1);
+        }
+        console.log(removed);
+
+      }
+    });
+
+
+    });
+
+    $("button#show").click(function() {
+      console.log("show");
+      $("#contactsList").text("");
+      for(var contactIndex = 0; contactIndex < contacts.length; contactIndex ++){
+        firstName = contacts[contactIndex].first;
+        lastName = contacts[contactIndex].last;
+        phoneNumber = contacts[contactIndex].number;
+        eMail = contacts[contactIndex].email;
+
+        $("#contact").clone().prependTo("#contactsList");
+        $("#contact").children("#name").text("Name: " + firstName + " " + lastName);
+        $("#moreInfo").children("#number").text("Number: " + phoneNumber);
+        $("#moreInfo").children("#email").text("Email: " + eMail);
+      }
+      });
 
     // contacts.forEach(function(contact) {
     //   console.log(contacts[last]);
@@ -41,6 +67,3 @@ $(function() {
     //   // $("#name").innerHTML(" " + contact.first + " " + contact.last);
     // });
   });
-
-
-});
